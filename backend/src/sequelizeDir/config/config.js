73490747eeaa0b24242c33dc81c307543d61a3f9
suhani2401 <patelsuhanik24@gmail.com>
2026-04-28 17:48:03 +0530

    added production sequelize config

--- backend/src/sequelizeDir/config/config.js
+++ backend/src/sequelizeDir/config/config.js
@@ -1,13 +1,27 @@
-// src/sequelizeDir/config/sequelize-cli.config.js
-require("dotenv").config({quiet: true});
+require("dotenv").config({ quiet: true });
 
 module.exports = {
   development: {
-    username: 'postgres',
-    password: 'test@123',
-    database: 'e-commerce',
-    host: 'localhost',
+    username: "postgres",
+    password: "test@123",
+    database: "e-commerce",
+    host: "localhost",
     port: 5432,
     dialect: "postgres",
+  },
+
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
